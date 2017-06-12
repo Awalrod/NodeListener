30cbfe73382aea342e901d1c083d91c2443aa527
Alexander Walrod <awalrod123@gmail.com>
2017-06-12 18:45:15 +0000

    Made a descriptive timestamp

--- DataFormatting/DataFormatter.java
+++ DataFormatting/DataFormatter.java
@@ -46,7 +46,7 @@ public class DataFormatter {
      * @return A header comment
      */
     public String produceHeader(int[] indexes){
-        String header = new String(";").concat(new SimpleDateFormat("HH:mm:ss").format(new Date()));
+        String header = new String(";").concat(new SimpleDateFormat("EEE d MMM yyyy HH:mm:ss:SS z").format(new Date()));
         for(int index : indexes){
             header = header.concat(","+Integer.toHexString(index)+"(X)");
             header = header.concat(","+Integer.toHexString(index)+"(Y)");
